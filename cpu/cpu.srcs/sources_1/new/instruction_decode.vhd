a49900b09a3b388d913a653bb736e542eff0bc8f
Tech-Matt <mattia.rizzo.un@gmail.com>
2024-05-21 01:08:29 +0200

    Implemented register file, instruction decode entity

--- cpu/cpu.srcs/sources_1/new/instruction_decode.vhd
+++ cpu/cpu.srcs/sources_1/new/instruction_decode.vhd
@@ -33,25 +33,31 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity instruction_decode is
   Port ( 
-        instr_in: in std_logic_vector(31 downto 0); -- Instruction to be decoded
-        rd_write_en: in std_logic; -- Write enable for register file
-        rd_value: in std_logic_vector(31 downto 0); -- Value to be eventually saved in register file
-        rs1_val: out std_logic_vector(31 downto 0); -- Register source value 1 
-        rs2_val: out std_logic_vector(31 downto 0); -- Register source value 2
-        immediate: out std_logic_vector(31 downto 0);
-        op_class: out std_logic_vector (4 downto 0);
-        alu_opcode: out std_logic_vector(2 downto 0); -- func 3
+        -- INPUTS
+        clk: in std_logic; 
+        pc: in unsigned(31 downto 0); -- Current Program counter (it is an input of 
+        next_pc: in unsigned(31 downto 0); -- Next program counter
+        instr: in std_logic_vector (31 downto 0); -- Instruction coming from Instruction Fetch stage
+        write_en: in std_logic; -- Write emable for the register file
+        rd_value: in unsigned(31 downto 0); -- Possible value to be written to register (chosen by next stages)
+        
+        -- OUTPUTS
+        rs_1: out unsigned(31 downto 0); -- Register source 1 value
+        rs_2: out unsigned (31 downto 0); -- Register source 2 value
+        immediate: out unsigned(31 downto 0); 
+        op_class: out std_logic_vector(4 downto 0); -- Operation type (Encoded ONE-HOT OSLBJ)
+        alu_opcode: out std_logic_vector(2 downto 0);
         a_sel: out std_logic;
         b_sel: out std_logic;
-        cond_opcode: out std_logic_vector(2 downto 0) -- func 3
         
-  
-  );
+        
+        );
 end instruction_decode;
 
 architecture Behavioral of instruction_decode is
 
-COMPONENT register_file
+-- Register File
+COMPONENT dist_mem_gen_0
   PORT (
     a : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
     d : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
@@ -63,8 +69,27 @@ COMPONENT register_file
   );
 END COMPONENT;
 
+-- Read Addresses
+signal ADDR_A: std_logic_vector(31 downto 0);
+signal ADDR_B: std_logic_vecctor(31 downto 0);
+-- Signals where instruction will be decoded
+signal OPCODE: std_logic_vector(6 downto 0);
+signal func7: std_logic_vector(7 downto 0);
+signal func3: std_logic_vector(3 downto 0);
+
+-------------------------------------------------------
 begin
 
+register_file: dist_mem_gen_0
+  PORT MAP (
+    a => ADDR_A, -- Read Address of RS_A
+    d => rd_value, -- Data input for write operations
+    dpra => ADDR_B, -- Read Address of RS_B
+    clk => clk,
+    we => write_en, -- Write enable
+    qspo => RS_A, -- RS_A Data
+    qdpo => RS_B -- RS_B Data
+  );
 
 
 end Behavioral;
